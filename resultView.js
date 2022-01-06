7969a3b0658d99fd08730ba5fcb39fa51d7cd12a
LinnOestgaard <linn.oes98@gmail.com>
2022-01-06 14:46:25 +0100

    FirstPush
    I skoletiden torsdag 06.01.22

--- resultView.js
+++ resultView.js
@@ -1,9 +1,23 @@
-    // [ ] Show question/title 
+    // [√] Show question/title 
     // [ ] Show number of answers
     // [ ] Show alternatives with number of times chosen
     // [ ] Create end/download button
     // [ ] Create logout button
 function resultView() {
+    let html = "";
 
+    //Finner riktig pollId
+    let curPollId = null;
+    for (let i = 0; i < model.polls.length; i++) {
+        if (model.app.pollID === model.polls[i].ID) {
+            curPollId = i;
+        }
+    }
+
+    //Vis titelen
+    html += /*html*/ `
+    <div>${(model.polls[curPollId].title)}</div>
+    `;
+
+    document.getElementById("app").innerHTML = html;
 }
-    
