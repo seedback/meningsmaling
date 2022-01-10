b00d64982abf2d814584791fd9ac6824640fba8b
LinnOestgaard <linn.oes98@gmail.com>
2022-01-10 10:07:11 +0100

    controllerForResult

---
+++ resultController.js
@@ -0,0 +1,17 @@
+
+function endPoll() {
+    model.polls.pollId,isEnded = true;
+    updateView();
+};
+
+function downloadPoll() {
+    if (model.polls.pollId.isEnded = true) {
+        //download osv
+    }
+
+};
+
+function logOut(){
+	model.app.userID = null;
+	updateView();
+};
